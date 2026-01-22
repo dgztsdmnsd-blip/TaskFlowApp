a0a80f85e533dad0403fe10ede9858b8997effaf
Luc Banchetti <banchetti.luc@gmail.com>
2026-01-22 09:45:01 +0100

    Initial Commit

---
+++ TaskFlow/TaskFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskFlowApp.swift
+//  TaskFlow
+//
+//  Created by luc banchetti on 22/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct TaskFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
