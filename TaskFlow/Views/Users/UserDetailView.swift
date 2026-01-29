1967cf41a896c915fb11b19ca3dc99ace8d87eeb
Luc Banchetti <banchetti.luc@gmail.com>
2026-01-29 18:02:24 +0100

    Admin

---
+++ TaskFlow/Views/Users/UserDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  UserDetailView.swift
+//  TaskFlow
+//
+//  Created by luc banchetti on 28/01/2026.
+//
+
+import SwiftUI
+
+struct UserDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UserDetailView()
+}
